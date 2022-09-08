e353e050f83ffea08da34a6cd3e980d2e993ac92
langatelkanah <elkanahlangatt@gmail.com>
2022-09-08 11:46:40 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string using printf
+ *
+ * Return: 0 is always returned
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
