2b41b0948eb8b625270da3f25e722f3ee9525234
langatelkanah <elkanahlangatt@gmail.com>
2022-09-15 18:46:10 +0000

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,7 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * print - print the word "_putchar"
- *
- * @a: store the string to be printed
- * @len: store the length of the string
- * @i: the iterable variable
  *
  * Return: Always return 0
  */
@@ -14,8 +9,9 @@ int print(void)
 {
 	char a[] = "_putchar";
 
-	int len = strlen(a);
+	int len = 8;
 	int i;
+
 	for (i = 0; i < len; i++)
 	{
 		_putchar(a[i]);
