2ed745d26fe820a56fe7824c10c4d220ca2b690c
langatelkanah <elkanahlangatt@gmail.com>
2022-09-16 08:44:37 +0000

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * more_numbers - Print numbers 0 to 14 10 times
+ */
+void more_numbers(void)
+{
+	int counter;
+	int num;
+
+	counter = 0;
+
+	while (counter < 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar((num % 10) + '0');
+		}
+
+		_putchar('\n');
+
+		counter++;
+	}
+}
