be764758a3b2186e4752623865ee172e2080a6c8
elkanahlangatt <elkanahlangatt@gmail.com>
2022-09-19 15:06:28 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value of a variable to 98
+ *
+ * @n: The address to be updated
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
