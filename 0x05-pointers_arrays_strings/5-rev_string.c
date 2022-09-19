389c52fd4587ab1d3a1403d3534ae6c4a44d6997
elkanahlangatt <elkanahlangatt@gmail.com>
2022-09-19 19:37:32 +0000

    used a for loop

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Print a string in reverse
+ *
+ * @s: The string to be printed
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int temp;
+	int len = 0;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (j = 0; j < len; j++)
+	{
+		len--;
+		temp = s[j];
+		s[j] = s[len];
+		s[len] = temp;
+	}
+}
