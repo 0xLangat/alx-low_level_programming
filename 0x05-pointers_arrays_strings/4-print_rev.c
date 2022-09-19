2181641dd32ccf84e779b2917af1f2208ed4a66c
elkanahlangatt <elkanahlangatt@gmail.com>
2022-09-19 18:46:18 +0000

    fixed size of length

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@
 void print_rev(char *s)
 {
 	int i;
-	int len = 0;
 	int j;
+	int len = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-		len = len + 1;
+		len++;
 
-	for (j = len; j >= 0; j--)
+	for (j = len - 1; j >= 0; j--)
 		_putchar(s[j]);
 
 	_putchar('\n');
