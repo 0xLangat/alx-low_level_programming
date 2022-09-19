3ea752d14b06effb702be37e967ee25e80b7ceea
elkanahlangatt <elkanahlangatt@gmail.com>
2022-09-19 19:49:58 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print every character of a string startin with a character
+ * followed by a new line
+ *
+ * @str: the string to be printed
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
