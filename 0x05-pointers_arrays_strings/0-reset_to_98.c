54edcafa48562eab2cd0a159f3c20491649b1676
DevOutlaw <elkanahlangatt@gmail.com>
2022-10-02 10:52:39 +0000

    Redoing the task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update value of a variable to 98
+ * reset_to_98 - Update the value to 98
  *
- * @n: The address to be updated
- *
- * Return: nothing
+ * @n: The address of the value to be changed
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,6 @@
-1: #Pointers, Arrays and Strings
+# 0x05. C - Pointers, arrays and strings
+
+## 0. 98 Battery st.
+Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+
+Prototype: void reset_to_98(int *n);
