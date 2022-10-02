a70b97bcf57d42d3bfa2e0bb3cdc953602f3f252
DevOutlaw <elkanahlangatt@gmail.com>
2022-10-02 12:16:06 +0000

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Print a string in reverse
+ *
+ * @s: The string
+ */
+void print_rev(char *s)
+{
+	int counter, i;
+
+	counter = 0;
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	for (i = counter; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
