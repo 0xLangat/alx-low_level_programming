01a960e855b29e3057ba36e8618a8bbbd7465928
DevOutlaw <elkanahlangatt@gmail.com>
2022-10-04 19:54:57 +0000

    consider 0 size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *str;
 
+	if (size == 0)
+		return (NULL);
+
 	str = (char *) malloc(size * sizeof(char));
 	if (str == NULL)
 		return (NULL);
