3f08baae723fe05b78fffd19121304f6a61c2454
DevOutlaw <elkanahlangatt@gmail.com>
2022-10-05 08:18:02 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: The pointer to the string
+ * @c: The char to be located
+ *
+ * Return: On success the pointer to the char
+ * On failure NULL
+ */
+char *_strchr(char *s, char c)
+{
+	for (; ; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+
+	return (NULL);
+}
