f7849326b8a2265615d495f5340ac4c26979e80a
DevOutlaw <elkanahlangatt@gmail.com>
2022-10-06 08:24:30 +0000

    fixed segmentation fault

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return a pointer to a 2 dimensional array
+ *
+ * @width: The width of the array (columns)
+ * @height: The height of the array (rows)
+ *
+ * Return: On success pointer to a 2 dimensional array
+ * On failure return NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **arr;
+
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int));
+
+	if (arr == 0)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr == 0)
+		{
+			free(arr);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
