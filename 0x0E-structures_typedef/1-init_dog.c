1fb412ca77c65b340499d413a8b4db92022a0480
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-10-12 06:18:37 +0000

    mem aloc

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct
@@ -11,6 +12,15 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		if (d == NULL)
+		{
+			return;
+		}
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
