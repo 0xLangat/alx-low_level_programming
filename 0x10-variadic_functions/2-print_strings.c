f3e7fc5aca6f88e25f47a68c9c1116b2588d8e1a
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-10-14 15:21:41 +0000

    check if null

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,13 +9,20 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *str;
 	va_list ap;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
+		str = va_arg(ap, char *);
+		if (str == NULL)
+		{
+			str = "(nil)";
+		}
+
+		printf("%s", str);
 
 		if (separator != NULL && i != (n - 1))
 		{
