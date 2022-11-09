db7c8f4bdf9ca5e89825faedf0aab01cbd533baa
Elkana Lang'at <elkanahlangatt@gmail.com>
2022-11-09 04:53:43 +0000

    sec commit

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,6 @@
-1: #Debugging in c
+# 0x03. C - Debugging
+
+## 0. Multiple mains
+## 1. Like, comment, subscribe
+## 2. 0 > 972
+## 3. Leap year
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
